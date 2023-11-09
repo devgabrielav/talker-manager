5a2c43fbe4bf0fd213efac8cba8062cd8e8d5c4d
devgabrielav <gabrivicentini@icloud.com>
2023-11-09 16:04:21 -0300

    Req 2 - Crie o endpoint GET /talker/:id, complete!

--- src/talker.js
+++ src/talker.js
@@ -6,14 +6,32 @@ const talkerRoutes = Router();
 
 const path = '/talker.json';
 
-talkerRoutes.get('/', async (req, res) => {
+async function readAll() {
   const allTalkers = await fs.readFile(join(__dirname, path), 'utf8');
   const parsed = JSON.parse(allTalkers);
+
+  return parsed;
+}
+
+talkerRoutes.get('/', async (req, res) => {
+  const allTalkers = await readAll();
 
   if (!allTalkers) {
     return res.status(200).json([]);
   }
-  return res.status(200).json(parsed);
+  return res.status(200).json(allTalkers);
+});
+
+talkerRoutes.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const allTalkers = await readAll();
+
+  const correctTalker = allTalkers.find((talker) => talker.id === Number(id));
+
+  if (correctTalker) {
+    return res.status(200).json(correctTalker);
+  }
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 module.exports = talkerRoutes;
